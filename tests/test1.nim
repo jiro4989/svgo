401458fd3dfc54649a6371c052ae1d3f998fad72
jiro4989 <jiroron666@gmail.com>
2020-03-01 20:59:18 +0900

    :art: nimpretty

--- tests/test1.nim
+++ tests/test1.nim
@@ -14,25 +14,26 @@ suite "parseAttrArg":
   test "cx=$NR == cx=1":
     check parseAttrArg("cx=$NR", 1, @["100", "200"]) == ("cx", "1")
   test "cx=$10 == cx=4":
-    check parseAttrArg("cx=$10", 1, @["99", "1", "1", "1", "1", "1", "1", "1", "1", "4"]) == ("cx", "4")
+    check parseAttrArg("cx=$10", 1, @["99", "1", "1", "1", "1", "1", "1", "1",
+        "1", "4"]) == ("cx", "4")
 
 suite "parseArgs":
   test "[ circle cx=100 cy=100 ]":
     var want = newElement("circle")
-    want.attrs = {"cx":"100", "cy":"100"}.toXmlAttributes
+    want.attrs = {"cx": "100", "cy": "100"}.toXmlAttributes
     var args = @["[", "circle", "cx=100", "cy=100", "]"]
     let got = parseArgs(args, 1, @[])
     check $want == $got
   test "[ circle cx=$1 cy=$2 ] and [100, 200]":
     var want = newElement("circle")
-    want.attrs = {"cx":"100", "cy":"200"}.toXmlAttributes
+    want.attrs = {"cx": "100", "cy": "200"}.toXmlAttributes
     var args = @["[", "circle", "cx=$1", "cy=$2", "]"]
     let got = parseArgs(args, 1, @["100", "200"])
     check $want == $got
   test "[ g [ circle cx=100 cy=100 ] ]":
     var want = newElement("g")
     var want2 = newElement("circle")
-    want2.attrs = {"cx":"100", "cy":"100"}.toXmlAttributes
+    want2.attrs = {"cx": "100", "cy": "100"}.toXmlAttributes
     want.add(want2)
     var args = @["[", "g", "[", "circle", "cx=100", "cy=100", "]", "]"]
     let got = parseArgs(args, 1, @[])
@@ -41,18 +42,20 @@ suite "parseArgs":
     var want = newElement("g")
     block:
       var sub = newElement("circle")
-      sub.attrs = {"cx":"100", "cy":"100"}.toXmlAttributes
+      sub.attrs = {"cx": "100", "cy": "100"}.toXmlAttributes
       want.add(sub)
     block:
       var sub = newElement("rect")
-      sub.attrs = {"x":"0", "y":"0", "width":"200","height":"200"}.toXmlAttributes
+      sub.attrs = {"x": "0", "y": "0", "width": "200",
+          "height": "200"}.toXmlAttributes
       want.add(sub)
-    var args = @["[", "g", "[", "circle", "cx=100", "cy=100", "]", "[", "rect", "x=0", "y=0", "width=200", "height=200", "]", "]"]
+    var args = @["[", "g", "[", "circle", "cx=100", "cy=100", "]", "[", "rect",
+        "x=0", "y=0", "width=200", "height=200", "]", "]"]
     let got = parseArgs(args, 1, @[])
     check $want == $got
   test "[ text x=10 y=20 z=$NR TEXT=HelloWorld ]":
     var want = newElement("text")
-    want.attrs = {"x":"10", "y":"20", "z":"1"}.toXmlAttributes
+    want.attrs = {"x": "10", "y": "20", "z": "1"}.toXmlAttributes
     want.add(newText("HelloWorld"))
     var args = @["[", "text", "x=10", "y=20", "z=$NR", "TEXT=HelloWorld", "]"]
     let got = parseArgs(args, 1, @[])
